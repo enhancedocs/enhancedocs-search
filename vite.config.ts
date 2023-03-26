3fea8b3e8f7c29ac325b76541211207be0e037ff
layolap <pol.layola@haufe.com>
2023-03-26 20:08:29 +0200

    revert: css module generateScopedName

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,6 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  css: {
-    modules: {
-      generateScopedName(name) {
-        return name;
-      }
-    }
-  },
   build: {
     lib: {
       name: 'enhancedocs-search',
