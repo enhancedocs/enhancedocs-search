bf4963676640917ba56bf0c9b893cb0e9ed2384e
layolap <pol.layola@haufe.com>
2023-04-06 14:59:25 +0200

    docs: improved search storybook examples

--- lib/Search.stories.tsx
+++ lib/Search.stories.tsx
@@ -2,6 +2,17 @@ import { useState } from 'react';
 import { ComponentMeta, ComponentStory } from '@storybook/react';
 import Search from './Search';
 
+const ENHANCED_SEARCH_CONFIG = {
+  projectId: '642c2d009557653a6d46cdda',
+  accessToken: 'pk_c237abe4951408b069e6482ad7b4214ea7ce6901bf699dbe'
+};
+
+const DOC_SEARCH_CONFIG = {
+  apiKey: 'VknaNXobIMS1DocvqohOZxDwfP4jqYO2',
+  host: '824vz69qrn0af371p-1.a1.typesense.net',
+  collection: 'ni61t27j9poqkf5bp_1680527568'
+};
+
 export default {
   title: 'Search',
   component: Search
@@ -35,17 +46,17 @@ const Template: ComponentStory<typeof Search> = (args) => {
   );
 };
 
-export const Example = Template.bind({});
-Example.args = {
+export const EnhancedSearch = Template.bind({});
+EnhancedSearch.args = {
   config: {
-    enhancedSearch: {
-      projectId: '642c2d009557653a6d46cdda',
-      accessToken: 'pk_c237abe4951408b069e6482ad7b4214ea7ce6901bf699dbe',
-    },
-    docSearch: {
-      apiKey: 'VknaNXobIMS1DocvqohOZxDwfP4jqYO2',
-      host: '824vz69qrn0af371p-1.a1.typesense.net',
-      collection: 'ni61t27j9poqkf5bp_1680527568'
-    }
+    enhancedSearch: ENHANCED_SEARCH_CONFIG
+  }
+};
+
+export const EnhancedAndDocSearch = Template.bind({});
+EnhancedAndDocSearch.args = {
+  config: {
+    enhancedSearch: ENHANCED_SEARCH_CONFIG,
+    docSearch: DOC_SEARCH_CONFIG
   }
 };
