12ae5c2aa5e64bd9f88e46b942a2e3b5372dcd37
layolap <pol.layola@haufe.com>
2023-04-22 09:34:56 +0200

    feat: render search method to allow using it in any JS file

--- lib/components/search-modal/services/instance.ts
+++ lib/components/search-modal/services/instance.ts
@@ -2,12 +2,12 @@ import { EnhancedSearchConfig } from '../../../Search';
 
 const BASE_URL = 'https://api.enhancedocs.com';
 
-export async function Get (route: string, config: EnhancedSearchConfig, options?: Object) {
+export async function Get (route: string, config: EnhancedSearchConfig, options?: object) {
   const baseURL = config.apiBaseURL || BASE_URL;
   return fetch(`${baseURL}${route}`, options).then((response) => response.json());
 }
 
-export async function Post (route: string, config: EnhancedSearchConfig, options?: Object) {
+export async function Post (route: string, config: EnhancedSearchConfig, options?: object) {
   const baseURL = config.apiBaseURL || BASE_URL;
   return fetch(`${baseURL}${route}`, { method: 'POST', ...options }).then((response) => response.json());
 }
